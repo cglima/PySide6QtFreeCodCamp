6c7393efbdf5a562851d8d388ee36be28f3c853a
cglima <lima.cglima@gmail.com>
2023-11-24 20:47:52 -0300

    organizando o código em classes - configurando todo o código no escopo global - configurando uma classe separada

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+""" 
+# VERSION1: Setting everything up in the global scope
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 import sys  
@@ -13,3 +16,25 @@ window.setCentralWidget(button)
 
 window.show()
 app.exec()
+
+"""
+
+# VERSION2: Setting up a separate class
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder App")
+        button = QPushButton("Press Me!")
+
+        #Set up the button as our central widget
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = ButtonHolder()
+window.show()
+app.exec()
+
