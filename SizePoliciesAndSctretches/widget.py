b9d43ba7ec68654819a9ccf79bdd1ef44cc77182
cglima <lima.cglima@gmail.com>
2023-11-26 19:14:54 -0300

    manipulando QSizePolicy e Stretches - adicionando botões na janela para ver como se comportam expandindo ou diminuindo

--- SizePoliciesAndSctretches/widget.py
+++ SizePoliciesAndSctretches/widget.py
@@ -5,7 +5,7 @@ class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Size policies")
+        self.setWindowTitle("Size policies and stretches")
 
         #Size policy: how the widgets behaves if container space is expanded or shrunk
         label = QLabel("Some text : ")
@@ -13,6 +13,7 @@ class Widget(QWidget):
 
         #Simulating the defaults. Show vertical expanding and appreciate how weird it is!
         line_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         #line_edit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         #Make the label expand horizontally
@@ -22,4 +23,21 @@ class Widget(QWidget):
         h_layout_1.addWidget(label)
         h_layout_1.addWidget(line_edit)
 
-        self.setLayout(h_layout_1)
+        button_1 = QPushButton("One")
+        button_2 = QPushButton("Two")
+        button_3 = QPushButton("Three")
+
+        #Stretch: how much of the available space (in the layout) is occupied by each widget.
+        #You specify the stretch when you add things to the layout: button_ takes up 2 units,
+        # button_2 and button_3 each take up 1 unit
+
+        h_layout_2 = QHBoxLayout()
+        h_layout_2.addWidget(button_1, 2)
+        h_layout_2.addWidget(button_2, 1)
+        h_layout_2.addWidget(button_3, 1)
+
+        v_layout = QVBoxLayout()
+        v_layout.addLayout(h_layout_1)
+        v_layout.addLayout(h_layout_2)
+
+        self.setLayout(v_layout)
